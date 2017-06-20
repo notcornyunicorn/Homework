1a32622cc141250ee995503cb78748036857491b
Анастасия Макарович <nastya7mak@gmail.com>
2017-06-21 00:23:50 +0300

    Add files via upload
    Почему-то в первом загруженном файле потерялся самый конец кода :с

--- ex.py
+++ ex.py
@@ -19,62 +19,62 @@ f = open('results.csv', 'r', encoding = 'utf-8')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, 'Привет! Я - бот, который спрашивает анкету по типологии признаков "острый - тупой".')
-	bot.send_message(message.chat.id, 'Я буду задавать тебе вопросы на русском языке. Напиши, пожалуйста, название языка, на котором ты будешь отвечать.')
-	with open('results.csv', 'a', encoding = 'utf-8') as results: 
-                results.write(message.text+'\n')
+		bot.send_message(message.chat.id, 'Привет! Я - бот, который спрашивает анкету по типологии признаков "острый - тупой".')
+		bot.send_message(message.chat.id, 'Я буду задавать тебе вопросы на русском языке. Напиши, пожалуйста, название языка, на котором ты будешь отвечать.')
+		with open('results.csv', 'a', encoding = 'utf-8') as results:
+			results.write(message.text+'\n')
 
-f = open(таблица, 'r', encoding = 'utf-8')
+f = open(r'/home/nastmak/mysite/table.txt', 'r', encoding = 'utf-8')
 table = f.read()
-words = table1.split('\n')
+words = table.split('\n')
 f.close()
 
 @bot.message_handler(func=lambda m: True)
-def send_len(message):        
-        i = 0
-        question1 = 'Можно ли на вашем языке употребить слово "тупой" вместе со словом ' + words1[i] + '?'
-        question2 = 'Можно ли на вашем языке употребить слово "острый" вместе со словом ' + words1[i] + '?'
-        keyboard = types.ReplyKeyboardMarkup(row_width=2)
-        btn1 = types.KeyboardButton('да')
-        btn2 = types.KeyboardButton('нет')
-        keyboard.add(btn1, btn2)
-        if i < len(words1):
-                tb.send_message(chat_id, question1, reply_markup=keyboard)
-                if KeyboardButton == bt1:
-                        bot.send_message(message.chat.id, 'Напиши, пожалуйста, как это будет на твоем языке.')
-                        with open('results.csv', 'a', encoding = 'utf-8') as results:
-                                results.write(message.text+'\n')
-                else:
-                        continue                
-        else:
-                tb.send_message(chat_id, question2, reply_markup=keyboard)
-                if KeyboardButton == bt1:
-                        bot.send_message(message.chat.id, 'Напиши, пожалуйста, как это будет на твоем языке.')
-                        with open('results.csv', 'a', encoding = 'utf-8') as results:
-                                results.write(message.text+'\n')
-                else:
-                        continue
-                i = i + 1
-        bot.send_message(message.chat.id, 'Чтобы посмотреть на свои ответы, набери \answers.')
+def send_len(message):
+		i = 0
+		question1 = 'Можно ли на вашем языке употребить слово "тупой" вместе со словом ' + words[i] + '?'
+		question2 = 'Можно ли на вашем языке употребить слово "острый" вместе со словом ' + words[i] + '?'
+		keyboard = types.ReplyKeyboardMarkup(row_width=2)
+		btn1 = types.KeyboardButton('да')
+		btn2 = types.KeyboardButton('нет')
+		keyboard.add(btn1, btn2)
+		if i < len(words):
+			tb.send_message(chat_id, question1, reply_markup=keyboard)
+			if KeyboardButton == bt1:
+				bot.send_message(message.chat.id, 'Напиши, пожалуйста, как это будет на твоем языке.')
+				with open('results.csv', 'a', encoding = 'utf-8') as results:
+					results.write(message.text+'\n')
+			else:
+				continue
+		else:
+			tb.send_message(chat_id, question2, reply_markup=keyboard)
+			if KeyboardButton == bt1:
+				bot.send_message(message.chat.id, 'Напиши, пожалуйста, как это будет на твоем языке.')
+				with open('results.csv', 'a', encoding = 'utf-8') as results:
+					results.write(message.text+'\n')
+			else:
+				i = i + 1
+		bot.send_message(message.chat.id, 'Чтобы посмотреть на свои ответы, набери \answers.')
 
 @bot.message_handler(commands=['answers'])
 def send_answers(message):
-        f = open('results.csv', 'r', encoding = 'utf-8')
-        answers = f.read()
-        f.close()
-	bot.send_message(message.chat.id, answers)
+		f = open('results.csv', 'r', encoding = 'utf-8')
+		answers = f.read()
+		f.close()
+		bot.send_message(message.chat.id, answers)
 
 
 @app.route('/', methods=['GET', 'HEAD'])
 def index():
-    return 'ok'
+	return 'ok'
 
 
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
+	if flask.request.headers.get('content-type') == 'application/json':
+		json_string = flask.request.get_data().decode('utf-8')
+		update = telebot.types.Update.de_json(json_string)
+		bot.process_new_updates([update])
+		return ''
+	else:
+		flask.abort(403)
